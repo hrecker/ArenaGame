5338f2b9954629581f0b4de034706102527b191e
hrecker <henryjrecker@gmail.com>
2018-07-15 23:21:06 -0500

    Added basic, bad enemy health script and destruction when hit enough

---
+++ Assets/Scripts/EnemyHealth.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyHealth : MonoBehaviour 
+{
+    public int health = 3;
+    public int maxHealth = 3;
+
+    void Start ()
+    {
+        health = maxHealth;
+    }
+
+    void OnCollisionEnter2D(Collision2D collision)
+    {
+        // Hit by player bullet
+        // TODO make this better
+        if (collision.gameObject.name.StartsWith("PlayerBullet"))
+        {
+            Destroy(collision.gameObject);
+            TakeDamage(1);
+        }
+    }
+
+    void TakeDamage(int damage)
+    {
+        health -= damage;
+        if (health < 0)
+        {
+            health = 0;
+        }
+        if (health == 0)
+        {
+            Destroy(gameObject);
+        }
+    }
+}
