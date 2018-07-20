571e9d22a552d93c939d61b97d9a193181e17cfd
hrecker <henryjrecker@gmail.com>
2018-07-20 12:04:26 -0500

    Added some randomization to enemy movement

--- Assets/Scripts/Movements/BasicEnemyMovement.cs
+++ Assets/Scripts/Movements/BasicEnemyMovement.cs
@@ -7,6 +7,8 @@ public class BasicEnemyMovement : MonoBehaviour
     public GameObject player;
     public float maxSpeed = 7.0f;
     public float accelerationScalar = 3.0f;
+    [Range(0, 1.0f)]
+    public float movementRandomization = 0.1f;
     private Vector2 currentVelocity = Vector2.zero;
     private Vector2 size = Vector2.zero;
 
@@ -25,6 +27,8 @@ public class BasicEnemyMovement : MonoBehaviour
         if (player != null)
         {
             Vector2 direction = player.transform.position - transform.position;
+            // Add some randomization to enemy movement
+            direction = RandomizeDirection(direction);
             Vector2 acceleration = direction.normalized * accelerationScalar;
             currentVelocity += (acceleration * Time.deltaTime);
 
@@ -41,5 +45,11 @@ public class BasicEnemyMovement : MonoBehaviour
         {
             this.enabled = false;
         }
+    }
+
+    private Vector2 RandomizeDirection(Vector2 startingDirection)
+    {
+        float angleRandomization = ((360 * Random.value) - 180) * movementRandomization;
+        return startingDirection.Rotate(angleRandomization);
     }
 }
